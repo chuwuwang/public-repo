aa01fd0e02c83d8a2a86c859194ad92256753821
chuwuwang <chuwuwang@126.com>
2026-08-05 18:03:05 +0800

    feat：修复编译问题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,4 +39,6 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
+
+    implementation( project(":background-library") )
 }
