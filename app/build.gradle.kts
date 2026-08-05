8c5f199a6930c68c0b60af2bebb819fe86d2eeef
chuwuwang <chuwuwang@126.com>
2026-08-05 19:30:11 +0800

    feat：删除 compose 的相关内容

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -21,7 +20,7 @@ android {
         }
     }
     buildFeatures {
-        compose = true
+        // compose = true
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -33,10 +32,10 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
 
-    implementation( platform(libs.androidx.compose.bom) )
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
+    // implementation( platform(libs.androidx.compose.bom) )
+    // implementation(libs.androidx.activity.compose)
+    // implementation(libs.androidx.compose.material3)
+    // implementation(libs.androidx.compose.ui)
+    // implementation(libs.androidx.compose.ui.graphics)
+    // implementation(libs.androidx.compose.ui.tooling.preview)
 }
